116fb6443e582fa76c667b7cb84bca1400f41ea6
SaiOngole <svo214@nyu.edu>
2014-04-24 12:15:59 -0400

    The app is working now

--- gen/com/is3av/text2speech/R.java
+++ gen/com/is3av/text2speech/R.java
@@ -995,7 +995,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1008,6 +1008,7 @@ containing a value of this type.
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
+        public static final int entertext=0x7f05003e;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
@@ -1038,6 +1039,7 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int speakbutton=0x7f05003d;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
--- src/com/is3av/text2speech/MainActivity.java
+++ src/com/is3av/text2speech/MainActivity.java
@@ -1,29 +1,57 @@
 package com.is3av.text2speech;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.util.Locale;
+
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity implements TextToSpeech.OnInitListener{
+	private EditText textEntered;
+	private TextToSpeech tts; 
+	private Button speak;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
+		tts = new TextToSpeech(this,this);
+		speak = (Button) findViewById(R.id.speakbutton);
+		textEntered = (EditText) findViewById(R.id.entertext);
+		speak.setOnClickListener(listener);
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
 
+	private OnClickListener listener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			speakOut();
+		}
+	} ;
+	@Override
+	public void onDestroy() {
+		//shutting down TTS
+		if(tts!=null) {
+			tts.stop();
+			tts.shutdown();
+		}
+		super.onDestroy();
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -61,4 +89,27 @@ public class MainActivity extends ActionBarActivity {
 		}
 	}
 
+	@Override
+	public void onInit(int status) {
+		// TODO Auto-generated method stub
+		if(status==TextToSpeech.SUCCESS) {
+			int result=tts.setLanguage(Locale.US);
+			if(result==TextToSpeech.LANG_MISSING_DATA||result==TextToSpeech.LANG_NOT_SUPPORTED) {
+				Log.e("TTS","This language is not supported");
+			}
+			else {
+				speak.setEnabled(true);
+				speakOut();
+			}
+		}
+			else {
+				Log.e("TTS","Initialization failed");
+			}
+		
+	}
+
+	private void speakOut() {
+		String text = textEntered.getText().toString();
+		tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+	}
 }
